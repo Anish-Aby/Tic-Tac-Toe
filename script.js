6e1eea0ff8be59c94a202b26129692292d833090
Anish-Aby <anishaby963@gmail.com>
2022-10-05 13:11:35 +0530

    Finished UI and started with backend.

---
+++ script.js
@@ -0,0 +1,11 @@
+let grid1 = document.querySelector(".one")
+
+document.addEventListener("click", (e) => {
+    let idk = e.target;
+    if (idk == ){
+        alert("Grid 1 working");
+    }
+})
+
+// div.grid.one
+//
