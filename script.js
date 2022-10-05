455c1842af4e577b59428af6c65d76e7388e9adf
Anish-Aby <anishaby963gmail.com>
2022-10-05 13:50:07 +0530

    Added animations.

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-let grid1 = document.querySelector(".one")
-
-document.addEventListener("click", (e) => {
-    let idk = e.target;
-    if (idk == ){
-        alert("Grid 1 working");
-    }
-})
-
 // div.grid.one
 //
+
+
+let tl = gsap.timeline({ defaults: { ease: "power4.inOut", duration: 1}});
+
+tl.to(".title-text", {'clip-path': 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)', y: 0, opacity: 1, duration: 3})
+tl.to(".buttons-container", { y: 0, opacity: 1, duration: 3}, 0)
+tl.to(".grid", {'clip-path': 'circle(111.8% at 50% 0%)', opacity: 1, duration: 3}, 0)
+
